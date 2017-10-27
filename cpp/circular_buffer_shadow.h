0a3dab2f0a0340776ceb30bd89a26a7e09e05884
Enrico Rossi <e.rossi@tecnobrain.com>
2017-10-27 14:49:49 +0200

    add shadow index template
      * and test example.

---
+++ cpp/circular_buffer_shadow.h
@@ -0,0 +1,163 @@
+/*
+ * Circular Buffer, an object oriented circular buffer.
+ * Copyright (C) 2015-2017 Enrico Rossi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#ifndef CBUFFER_SHADOW_H
+#define CBUFFER_SHADOW_H
+
+#include "circular_buffer.h"
+
+/*! Buffer structure
+ 
+ [ | | | | | | | | | | | | | | | | | | | | | | | | ]
+  ^buffer   ^start    ^shadow_start       ^idx    ^TOP
+                      ^ --- shadow_len ---^
+            ^------------ len ------------^
+  ^---------------------- size -------------------^
+ */
+
+template <typename T, typename D>
+class CBufferS : public CBuffer<T, D> {
+	private:
+		T shadow_start_;
+		T shadow_len_;
+	public:
+		// Debugging methods
+		T start() const { return(shadow_start_); };
+		T len() const { return(shadow_len_); };
+
+		// Contructor
+		// force the Base constructor with the parameter size
+		// or it will call the default with no parameters and
+		// it will throw and error if initialiazed with one.
+		CBufferS(T size = CBUF_SIZE);
+
+		// declaration overload for shadow index.
+		void clear();
+		bool popc(D*);
+		T pop(D*, const T);
+		bool push(D);
+
+		// new member functions
+		void commit();
+		void reset();
+};
+
+//! Clear the buffer and the shadow index.
+//
+// \sameas CBuffer::clear()
+//
+template <typename T, typename D>
+void CBufferS<T, D>::clear()
+{
+	CBuffer<T, D>::clear(); // call the base clear
+	shadow_start_ = 0;
+	shadow_len_ = 0;
+}
+
+//! Contruct the buffer with the shadow index.
+template <typename T, typename D>
+CBufferS<T, D>::CBufferS(T size) : CBuffer<T, D>{size}
+{
+	shadow_start_ = 0;
+	shadow_len_ = 0;
+}
+
+/*! Extract a single shadow object from the buffer.
+ *
+ * \warning possible race condition!
+ */
+template <typename T, typename D>
+bool CBufferS<T, D>::popc(D *data)
+{
+	if (shadow_len_) {
+		// Here "this->" could be used since operator[] has not
+		// been overloaded.
+		*data = CBuffer<T, D>::operator[](shadow_start_);
+
+		// Here "this->" could be used since TOP_ has not
+		// been overloaded.
+		if (shadow_start_ == CBuffer<T, D>::TOP_)
+			shadow_start_ = 0;
+		else
+			shadow_start_++;
+
+		// From here possible race condition with push().
+		shadow_len_--;
+
+		return (true);
+	} else {
+		return (false);
+	}
+}
+
+/*! Pop a copy of everything present in the buffer.
+ *
+ * \warning if the data size is less than the buffer, only the sizeofdata
+ * byte get fetched, the buffer remain not empty.
+ */
+template <typename T, typename D>
+T CBufferS<T, D>::pop(D* data, const T sizeofdata)
+{
+	T j {0};
+
+	// while there is space left on data and byte in the buffer
+	while ((j < sizeofdata) && popc(data + j))
+		j++;
+
+	return (j);
+}
+
+/*! add data to the buffer and update the shadow indexes.
+ *
+ * \warning race condition with other functions.
+ *  modified CBufferS members data: shadow_len_
+ */
+template <typename T, typename D>
+bool CBufferS<T, D>::push(D c)
+{
+	if (CBuffer<T, D>::push(c)) {
+		shadow_len_++;
+		return(true);
+	} else {
+		return(false);
+	}
+}
+
+/*! Commit the shadow index operations.
+ *
+ * Uses the protected CBuffer member functions.
+ */
+template <typename T, typename D>
+void CBufferS<T, D>::commit()
+{
+	CBuffer<T, D>::set_start(shadow_start_);
+	CBuffer<T, D>::set_len(shadow_len_);
+}
+
+/*! Restore the index back.
+ *
+ */
+template <typename T, typename D>
+void CBufferS<T, D>::reset()
+{
+	shadow_start_ = CBuffer<T, D>::start();
+	shadow_len_ = CBuffer<T, D>::len();
+}
+
+#endif
---
+++ cpp/test_shadow.cpp
@@ -0,0 +1,152 @@
+/*
+ * Circular Buffer, an object oriented circular buffer.
+ * Copyright (C) 2015-2017 Enrico Rossi
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include <iostream>
+#include <cstdio>
+#include "circular_buffer_shadow.h"
+
+#define MSG_SIZE 10 // message size less than buffer!
+#define BUF_SIZE 15 // buffer size
+
+using namespace std;
+
+void help()
+{
+	cout << "Usage keys:" << endl;
+	cout << " h : This help message." << endl;
+	cout << " a : Get ALL data from the buffer." << endl;
+	cout << " g : Get next object from the buffer." << endl;
+	cout << " c : Clear the buffer." << endl;
+	cout << " r : Reset the shadow index." << endl;
+	cout << " y : Commit the shadow index." << endl;
+	cout << " q : Quit." << endl;
+	cout << " CR : Do nothing." << endl;
+	cout << " <any others key> : Put a char in the buffer." << endl;
+	cout << endl;
+	cout << " \".\"   : buffer's slot empty." << endl;
+	cout << " [1..f]: slot used." << endl;
+	cout <<  endl;
+}
+
+// Print the content of the buffer and indexes.
+// \note: cout << unsigned char does not print numbers.
+void printit(const CBufferS<uint8_t, uint8_t>& cbuffer)
+{
+	printf("\n");
+	printf("i: %d | ", cbuffer.index());
+	printf("s: %d | ", cbuffer.start());
+	printf("l: %d | ", cbuffer.len());
+	printf("o: %d\n", cbuffer.overflow());
+	printf("\n");
+
+	for (uint8_t i = 0; i < cbuffer.size(); i++) {
+		if (cbuffer.index() < cbuffer.start()) {
+			if ((i >= cbuffer.start()) || (i < cbuffer.index()))
+				printf("%1x", i);
+			else
+				printf(".");
+		} else if (cbuffer.index() > cbuffer.start()) {
+			if ((i >= cbuffer.start()) && (i < cbuffer.index()))
+				printf("%1x", i);
+			else
+				printf(".");
+		} else {
+			if (cbuffer.len())
+				printf("%1x", i);
+			else
+				printf(".");
+		}
+	}
+
+	printf("\n");
+}
+
+int main() {
+	CBufferS<uint8_t, uint8_t> cbuffer {BUF_SIZE};
+	uint8_t *message;
+	uint8_t len;
+	bool FLloop {true};
+	uint8_t rxc;
+
+	message = new uint8_t[MSG_SIZE];
+	cout << endl << "Test circular buffer (shadow index)." << endl;
+	cout << "Copyright (C) 2015-2017 Enrico Rossi - GNU GPL" << endl;
+	cout << endl << "Fill the buffer with chars from a to e and" << endl;
+	cout << "get them one by one or all at once." << endl << endl;
+
+	help();
+
+	while (FLloop) {
+		/* get the char */
+		rxc = (uint8_t)getchar();
+
+		switch(rxc) {
+			case 'a':
+				len = cbuffer.pop(message, MSG_SIZE);
+
+				if (len) {
+					cout << "> Objects fetched: ";
+					cout << (int)len << " [";
+
+					for (auto i = 0; i < len; i++)
+						cout << *(message + i);
+
+					cout << "]" << endl;
+				} else {
+					cout << "> No data" << endl;
+				}
+
+				break;
+			case 'g':
+				if (cbuffer.popc(message)) {
+					cout << "> Single object fetched: ";
+					cout << message[0] << endl;
+				} else {
+					cout << "> No data." << endl;
+				}
+
+				break;
+			case 'h':
+				help();
+				break;
+			case 'q':
+				FLloop = false;
+				break;
+			case 'r':
+				cbuffer.reset();
+				break;
+			case 'c':
+				cbuffer.clear();
+				break;
+			case 'y':
+				cbuffer.commit();
+				break;
+			case '\n':
+				printit(cbuffer);
+				break;
+			default:
+				rxc = 'a' + cbuffer.index();
+				cbuffer.push(rxc);
+		}
+	}
+
+	delete[] message;
+
+	return(0);
+}
